46e9f9007a6595f75f376bc4380773fbfc480da6
rayba120 <rayba120@tlvm-4-3-1.ad.liu.se>
2021-12-03 10:29:34 +0100

    added

--- 7.nis/tests.py
+++ 7.nis/tests.py
@@ -2,22 +2,28 @@ import subprocess
 
 
 def test_domain():
-    domain = subprocess.run(['domainname | grep nis.example.com'],stdout=subprocess.DEVNULL,
-    stderr=subprocess.STDOUT,shell = True)
+    domain = subprocess.run(['ypdomainname | grep nis.example.com'],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
     assert domain.returncode == 0
 
-def test_services():
-    services = subprocess.run(['rpcinfo -p server| grep \'ypserv\|ypbind\|fypxfrd\|yppasswdd\''],stdout=subprocess.DEVNULL,
-    stderr=subprocess.STDOUT,shell = True)
+def test_ypserv():
+    services = subprocess.run(["rpcinfo -p server| grep ypserv"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
+    assert services.returncode == 0
+def test_ypbind():
+    services = subprocess.run(["rpcinfo -p server| grep ypbind"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
+    assert services.returncode == 0
+def test_fypxfrd():
+    services = subprocess.run(["rpcinfo -p server| grep fypxfrd"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
     assert services.returncode == 0
 
+def test_yppasswd():
+    services = subprocess.run(["rpcinfo -p server| grep yppasswd"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
+    assert services.returncode == 0
+    
 def test_nsswitch():
-    nsswitch = subprocess.run(["cat /etc/nsswitch.conf | grep nis | grep 'passwd\|group'"],stdout=subprocess.DEVNULL,
-    stderr=subprocess.STDOUT,shell = True)
+    nsswitch = subprocess.run(["cat /etc/nsswitch.conf | grep nis | grep 'passwd\\|group\\|gshadow\\|shadow\\|netgroup'"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell = True)
     assert nsswitch.returncode == 0
 
+def test_yptest():
+    yptest = subprocess.run(['yptest'],shell = True)
+    assert yptest.returncode == 1
 
-
-test_domain()
-test_services()
-test_nsswitch()
--- 7.nis/testserver.py
+++ 7.nis/testserver.py
@@ -6,6 +6,8 @@ def test_directory1():
     directory = subprocess.run(['ypmatch -x'],shell = True)
     assert directory.returncode == 0
 
+def test_yptest():
+    yptest = subprocess.run(['yptest'],shell = True)
+    assert yptest.returncode == 1
 
-#test_directory()
 test_directory1()
